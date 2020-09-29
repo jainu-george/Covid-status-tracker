b518131b6bfdb066b27d973cfe376d30b41bea1d
Jainu George <jainugeorge56@gmail.com>
2020-09-29 20:30:38 +1300

    project created

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {
+  BrowserRouter as Router,
+  Link,
+  Route,
+  Switch
+} from 'react-router-dom';
+import Header from './Components/Header';
+import Country from './Components/Country';
+import Statedata from './Components/Statedata';
+import World from './Components/World';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export default class App extends Component{
+  constructor(){
+    super()
+  }
+  render(){
+    return(
+      <div className="container-fluid">
+        <Router>
+          <Header/>
+          <Switch>
+            <Route exact path="/">
+              <Country/>
+            </Route>
+            <Route path="/country">
+              <Country/>
+            </Route>
+
+            <Route path="/statedata">
+              <Statedata/>
+            </Route>
+            <Route path="/world">
+              <World/>
+            </Route>
+            
+
+
+          </Switch>
+       
+          </Router>
+      </div>
+    )
+  }
 }
-
-export default App;
---
+++ src/Components/Header.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+import {Navbar,Nav}  from 'react-bootstrap';
+import {Link} from 'react-router-dom';
+
+export default class Header extends Component {
+    render() {
+        return (
+            <div>
+                <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+  <Navbar.Brand href="#home">Covid-19</Navbar.Brand>
+  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+  <Navbar.Collapse id="responsive-navbar-nav">
+    <Nav className="mr-auto">
+      <Link className="nav-link" to ="/country">India</Link>
+      <Link className="nav-link" to ="/world">World</Link>
+    
+    </Nav>
+   
+  </Navbar.Collapse>
+</Navbar>
+            </div>
+        )
+    }
+}
